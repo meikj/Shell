a6031bab7f5ac0e605cb2b1340abe39720cfe7d9
Andrew Logan <andy11619@hotmail.co.uk>
2013-02-12 11:34:40 +0000

    Exit part

--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	char buffer[512];
 	
 	while(1) {
 		printf("$ ");
-		//fgets(buffer, 512, stdin);
+		if(fgets(buffer, 512, stdin) == NULL){ 
+		   perror ("Error, Standard input closed");
+	       return 0;}
+		strtok (buffer,",.-");
+	    printf ("%s\n", buffer);
+	    if(strncmp (buffer,"Exit",4) ==0){
+		  return 0;}
 	}
 	
 	return 0;
