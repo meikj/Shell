51106b05950da354da446b1fc72649d9c3523197
John Meikle <john.meikle31@gmail.com>
2013-02-12 13:35:24 +0000

    Improved argument/token handling substantially
    Created a parse_args() function which takes in an array of token strings
    and parses them accordingly. This will make subsequent stages FAR easier,
    and allows the shell to be more extendable. For further command
    implementations, please add them to parse_args() in the style of the
    "exit" command.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+/* Parse the arguments/tokens from user input
+   
+   Params:
+	argc - Argument count (i.e. number of tokens)
+	argv - Pointer to an array containing input tokens
+ */
+void parse_args(int argc, char *argv[]) {
+	// Use the first token as indication of what to do (e.g. exit, ls, etc.)
+	if(strncmp(argv[0], "exit", 4) == 0) {
+		// exit command called
+		exit(0);
+	} else {
+		// An unsupported/invalid command was passed
+		printf("Invalid command called. Please use 'help' for more information.\n");
+	}
+	return;
+}
 
 int main(int argc, char *argv[]) {
 	char buffer[512];
-	char *token;
+	
+	// Store tokens here (um, for the time being statically allocate 10 possible tokens)
+	char *token_list[10];
+	int token_count = 0;
 	
 	while(1) {
 		printf("$ ");
 		
 		if(fgets(buffer, 512, stdin) == NULL) {
 			// stdin stream closed
-			perror("Error: stdin stream closed");
+			perror("stdin stream closed");
 			exit(1);
 		}
 		
-		token = strtok(buffer, " ");
+		// Store token pointer in token_list
+		token_list[token_count] = strtok(buffer, " ");
 		
-		while(token != NULL) {
-			printf("token = %s\n", token);
-			token = strtok(NULL, " ");
+		while(token_list[token_count] != NULL) {
+			printf("token_list[%d] = %s\n", token_count, token_list[token_count]);
+			token_count++;
+			token_list[token_count] = strtok(NULL, " ");
 		}
 		
-		if(strncmp(buffer, "exit", 4) == 0) {
-			// exit command called, quit program
-			return 0;
-		}
+		parse_args(token_count, token_list);
+		token_count = 0;
 	}
 	
 	return 0;
