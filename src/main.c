f4fa8c8566ec1c7bc846129be4387e9c86adb8d3
John Meikle <john.meikle31@gmail.com>
2013-02-12 17:10:12 +0000

    Improved documentation and naming
    This should make it easier to follow and understand. I've added comments
    and documentation to the code which explains each part. Also, I've added a
    help command which lists the available commands - right now it's just help
    and exit as per stage one.

--- src/main.c
+++ src/main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Parse the arguments/tokens from user input
+/* Parse the tokenized input and perform the relevant and appropriate operation(s)
    
    Params:
-	argc - Argument count (i.e. number of tokens)
-	argv - Pointer to an array containing input tokens
+	token_count - The number of tokens in the array
+	token_list - The array of token strings
  */
-void parse_args(int argc, char *argv[]) {
+void parse_tokens(int token_count, char *token_list[]) {
 	// Use the first token as indication of what to do (e.g. exit, ls, etc.)
-	if(strncmp(argv[0], "exit", 4) == 0) {
+	if(strncmp(token_list[0], "exit", 4) == 0) {
 		// exit command called
 		exit(0);
+	} else if(strncmp(token_list[0], "help", 4) == 0) {
+		// help command called
+		printf("help\tlist the available shell commands\n");
+		printf("exit\texit the shell\n");
 	} else {
 		// An unsupported/invalid command was passed
 		printf("Invalid command called. Please use 'help' for more information.\n");
@@ -21,6 +25,7 @@ void parse_args(int argc, char *argv[]) {
 }
 
 int main(int argc, char *argv[]) {
+	// User input buffer
 	char buffer[512];
 	
 	// Store tokens here (um, for the time being statically allocate 10 possible tokens)
@@ -31,21 +36,23 @@ int main(int argc, char *argv[]) {
 		printf("$ ");
 		
 		if(fgets(buffer, 512, stdin) == NULL) {
-			// stdin stream closed
+			// stdin stream closed, can't continue
 			perror("stdin stream closed");
 			exit(1);
 		}
 		
-		// Store token pointer in token_list
+		// Tokenize the user input and store the tokens in an array for easy parsing
 		token_list[token_count] = strtok(buffer, " ");
 		
 		while(token_list[token_count] != NULL) {
+			// There's still more tokens to get
 			printf("token_list[%d] = %s\n", token_count, token_list[token_count]);
 			token_count++;
 			token_list[token_count] = strtok(NULL, " ");
 		}
 		
-		parse_args(token_count, token_list);
+		// Now parse the token(s) and reset the counter
+		parse_tokens(token_count, token_list);
 		token_count = 0;
 	}
 	
